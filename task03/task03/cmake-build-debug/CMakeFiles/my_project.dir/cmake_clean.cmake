0b2187aae4ade2f439e6259efff0d6472009f203
Inna Tarasyuk <kysarat01@gmail.com>
2020-11-17 23:40:34 +0300

    task03

---
+++ task03/task03/cmake-build-debug/CMakeFiles/my_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_project.dir/main.cpp.obj"
+  "my_project.exe"
+  "my_project.exe.manifest"
+  "my_project.lib"
+  "my_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
